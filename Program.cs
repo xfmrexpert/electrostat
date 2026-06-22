d8cbbb7c803710216770c9bcfadb4a633b64365f
Timothy C. Raymond <tcraymond@xfmrexpert.com>
2026-06-22 18:39:16 -0400

    Remove Examples.cs and ExampleExporter tool

--- Program.cs
+++ Program.cs
@@ -1,48 +1,10 @@
-using System.Collections.Generic;
-
 namespace electrostat
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello, World!");
-            System.Console.WriteLine("Building electrostatic models with domain clipping enabled...\n");
-
-            foreach (bool withLVCornerAngle in new[] { true })
-            {
-                foreach (var tx in Examples.All(withLVCornerAngle))
-                {
-                    foreach (var cut in tx.Cuts)
-                    {
-                        var ex = tx.ResolveCut(cut);
-                        System.Console.WriteLine($"\n=== Building {tx.Name} / {cut.Name} ===");
-
-                        string caseName = SafeName(tx.Name) + "/" + SafeName(cut.Name);
-                        if (!withLVCornerAngle) caseName += "/noLVcorner";
-
-                        var model = GeometryBuilder.BuildModel(
-                            ex,
-                            lc: 5.0,
-                            mshOut: $"{caseName}/geom.msh",
-                            clipToDomain: true);
-                        model.Solve();
-                        return;
-                    }
-                }
-            }
-
-            System.Console.WriteLine("\n=== All models built successfully with domain clipping! ===");
-        }
-
-        private static string SafeName(string name)
-        {
-            var sb = new System.Text.StringBuilder(name.Length);
-            foreach (var c in name)
-            {
-                sb.Append(char.IsLetterOrDigit(c) ? c : '_');
-            }
-            return sb.ToString();
+            System.Console.WriteLine("electrostat");
         }
     }
 }
